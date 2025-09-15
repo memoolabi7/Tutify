7f6d0e00c7eece0155b412ecfd03083ef866bfdb
memoolabi7 <olabimm77@gmail.com>
2025-09-15 18:40:02 +0300

    set default app.run() options

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ def main():
     return render_template('main.html', title='Tutify', subject=subject)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run()
